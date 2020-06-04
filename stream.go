c5707efe68266e4853d8731b1919a2f15b3a6f53
hunterlong <info@socialeck.com>
2020-06-04 02:05:12 -0700

    ok

--- stream.go
+++ stream.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	channels  int = 1                   // 1 for mono, 2 for stereo
+	channels  int = 2                   // 1 for mono, 2 for stereo
 	frameRate int = 48000               // audio sampling rate
 	frameSize int = 960                 // uint16 size of each audio frame
 	maxBytes  int = (frameSize * 2) * 2 // max size of opus data
